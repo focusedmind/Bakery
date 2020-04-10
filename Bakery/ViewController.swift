9b55096648160ef54714b5a165f49333e33cd547
iOS Developer <ardashes@webant.ru>
2020-04-10 13:21:33 +0300

    Initial Commit

---
+++ Bakery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bakery
+//
+//  Created by iOS Developer on 4/10/20.
+//  Copyright © 2020 Bakery Union. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
